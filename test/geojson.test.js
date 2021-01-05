f9416c71cd641f7db69d114506a7d1420543e4f7
Owen Pearson <owen.pearson@ably.com>
2021-01-05 11:12:51 +0000

    amend directory structure in test-resources/geo

--- test/geojson.test.js
+++ test/geojson.test.js
@@ -4,17 +4,16 @@ const path = require('path');
 const jsonschema = require('jsonschema');
 
 const geoDir = path.resolve(__dirname, '..', 'test-resources', 'geo');
-const exampleDir = path.resolve(geoDir, 'examples');
-const examples = fs.readdirSync(exampleDir);
+const examples = fs.readdirSync(geoDir);
 const schema = require(path.resolve(geoDir, 'schema.json'));
 
 describe('GeoJson schema', () => {
-  examples.forEach(example => {
-    const instance = JSON.parse(fs.readFileSync(path.resolve(exampleDir, example)));
+  examples.filter(fileName => fileName !== 'schema.json').forEach(fileName => {
+    const instance = JSON.parse(fs.readFileSync(path.resolve(geoDir, fileName)));
     
-    it(example, () => {
+    it(fileName, () => {
       const { errors } = jsonschema.validate(instance, schema);
-      if (example.split('-')[0] === 'invalid') {
+      if (fileName.split('-')[0] === 'invalid') {
         expect(errors.length).to.be.greaterThan(0);
       } else {
         expect(errors.length).to.equal(0);
