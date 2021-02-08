11ef30b7a9b03adb8d5d34a6fe436f445d2bcdd3
Owen Pearson <owen.pearson@ably.com>
2021-02-08 14:29:32 +0000

    run prettier on all files

--- test/journey.test.js
+++ test/journey.test.js
@@ -1,30 +1,30 @@
-const expect = require("chai").expect;
-const fs = require("fs");
-const path = require("path");
-const { Validator } = require("jsonschema");
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const { Validator } = require('jsonschema');
 
-const geoDir = path.resolve(__dirname, "..", "test-resources", "geo");
+const geoDir = path.resolve(__dirname, '..', 'test-resources', 'geo');
 const exampleDir = path.resolve(geoDir, 'journeys');
 const examples = fs.readdirSync(exampleDir);
-const schema = require(path.resolve(geoDir, "journey-schema.json"));
+const schema = require(path.resolve(geoDir, 'journey-schema.json'));
 
 const jsonschema = new Validator();
-jsonschema.addSchema(JSON.parse(fs.readFileSync(path.resolve(geoDir, 'location-schema.json'))), 'https://schemas.ably.com/json/asset-tracking-common/Location');
+jsonschema.addSchema(
+  JSON.parse(fs.readFileSync(path.resolve(geoDir, 'location-schema.json'))),
+  'https://schemas.ably.com/json/asset-tracking-common/Location'
+);
 
-describe("Journey schema", () => {
-  examples
-    .forEach((fileName) => {
-      const instance = JSON.parse(
-        fs.readFileSync(path.resolve(exampleDir, fileName))
-      );
+describe('Journey schema', () => {
+  examples.forEach((fileName) => {
+    const instance = JSON.parse(fs.readFileSync(path.resolve(exampleDir, fileName)));
 
-      it(fileName, () => {
-        const { errors } = jsonschema.validate(instance, schema);
-        if (fileName.split("-")[0] === "invalid") {
-          expect(errors.length).to.be.greaterThan(0);
-        } else {
-          expect(errors.length).to.equal(0);
-        }
-      });
+    it(fileName, () => {
+      const { errors } = jsonschema.validate(instance, schema);
+      if (fileName.split('-')[0] === 'invalid') {
+        expect(errors.length).to.be.greaterThan(0);
+      } else {
+        expect(errors.length).to.equal(0);
+      }
     });
+  });
 });
--- test/location.test.js
+++ test/location.test.js
@@ -1,27 +1,24 @@
-const expect = require("chai").expect;
-const fs = require("fs");
-const path = require("path");
-const jsonschema = require("jsonschema");
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const jsonschema = require('jsonschema');
 
-const geoDir = path.resolve(__dirname, "..", "test-resources", "geo");
+const geoDir = path.resolve(__dirname, '..', 'test-resources', 'geo');
 const exampleDir = path.resolve(geoDir, 'locations');
 const examples = fs.readdirSync(exampleDir);
-const schema = require(path.resolve(geoDir, "location-schema.json"));
+const schema = require(path.resolve(geoDir, 'location-schema.json'));
 
-describe("Location schema", () => {
-  examples
-    .forEach((fileName) => {
-      const instance = JSON.parse(
-        fs.readFileSync(path.resolve(exampleDir, fileName))
-      );
+describe('Location schema', () => {
+  examples.forEach((fileName) => {
+    const instance = JSON.parse(fs.readFileSync(path.resolve(exampleDir, fileName)));
 
-      it(fileName, () => {
-        const { errors } = jsonschema.validate(instance, schema);
-        if (fileName.split("-")[0] === "invalid") {
-          expect(errors.length).to.be.greaterThan(0);
-        } else {
-          expect(errors.length).to.equal(0);
-        }
-      });
+    it(fileName, () => {
+      const { errors } = jsonschema.validate(instance, schema);
+      if (fileName.split('-')[0] === 'invalid') {
+        expect(errors.length).to.be.greaterThan(0);
+      } else {
+        expect(errors.length).to.equal(0);
+      }
     });
+  });
 });
