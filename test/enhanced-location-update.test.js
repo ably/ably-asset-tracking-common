7c85abebe7b410fae56a3e5d94170ef8f51f7be9
Kacper Kluka <k.kluka@nomtek.com>
2021-04-22 11:54:22 +0200

    Add schema for the EnhancedLocationUpdate

---
+++ test/enhanced-location-update.test.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const { Validator } = require('jsonschema');
+
+const geoDir = path.resolve(__dirname, '..', 'test-resources', 'geo');
+const exampleDir = path.resolve(geoDir, 'enhanced-location-updates');
+const examples = fs.readdirSync(exampleDir);
+const schema = require(path.resolve(geoDir, 'enhanced-location-update-schema.json'));
+
+const jsonschema = new Validator();
+jsonschema.addSchema(
+  JSON.parse(fs.readFileSync(path.resolve(geoDir, 'location-schema.json'))),
+  'https://schemas.ably.com/json/asset-tracking-common/Location'
+);
+
+describe('Journey schema', () => {
+  examples.forEach((fileName) => {
+    const instance = JSON.parse(fs.readFileSync(path.resolve(exampleDir, fileName)));
+
+    it(fileName, () => {
+      const { errors } = jsonschema.validate(instance, schema);
+      if (fileName.split('-')[0] === 'invalid') {
+        expect(errors.length).to.be.greaterThan(0);
+      } else {
+        expect(errors.length).to.equal(0);
+      }
+    });
+  });
+});
