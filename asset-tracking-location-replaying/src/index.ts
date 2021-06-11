17b4190fa3e5c5d81d69ef102d081ca5f2088646
Kacper Kluka <k.kluka@nomtek.com>
2021-06-11 14:23:04 +0200

    Add support for publishing messages to a destination channel

--- asset-tracking-location-replaying/src/index.ts
+++ asset-tracking-location-replaying/src/index.ts
@@ -20,39 +20,56 @@ program.parse(process.argv);
 const opts = program.opts();
 
 const configurationData: ConfigurationFileData = getDataFromFile(opts.configuration);
-const credentialsData: AblyCredentialsFileData = getDataFromFile(configurationData.sourceCredentials);
+if (
+  (configurationData.destinationCredentials && !configurationData.destinationTrackableId) ||
+  (!configurationData.destinationCredentials && configurationData.destinationTrackableId)
+) {
+  console.error('You need to specify both destination credentials and destination trackable ID.');
+  process.exit(1);
+}
+const isDestinationSpecified = configurationData.destinationCredentials && configurationData.destinationTrackableId;
+const sourceCredentialsData: AblyCredentialsFileData = getDataFromFile(configurationData.sourceCredentials);
+const destinationCredentialsData: AblyCredentialsFileData = isDestinationSpecified
+  ? getDataFromFile(configurationData.destinationCredentials || '')
+  : null;
 
 if (opts.verbose) {
   console.log('Configuration file:', opts.configuration);
   console.log(configurationData);
-  console.log('Credentials file:', configurationData.sourceCredentials);
-  console.log(credentialsData);
+  console.log('Source credentials file:', configurationData.sourceCredentials);
+  console.log(sourceCredentialsData);
+  console.log('Destination credentials file:', configurationData.destinationCredentials);
+  console.log(destinationCredentialsData);
 }
 (async () => {
-  let ably = new Ably.Realtime({
-    key: credentialsData.ablyApiKey,
-    clientId: CLIENT_ID,
-  });
+  let sourceAbly = new Ably.Realtime({ key: sourceCredentialsData.ablyApiKey, clientId: CLIENT_ID });
+  let destinationAbly = isDestinationSpecified
+    ? new Ably.Realtime({ key: sourceCredentialsData.ablyApiKey, clientId: CLIENT_ID })
+    : sourceAbly;
 
-  let channel = ably.channels.get(`${CHANNEL_PREFIX}${configurationData.sourceTrackableId}`);
-  if (opts.verbose) console.log('Channel:', channel.name);
+  let sourceChannel = sourceAbly.channels.get(`${CHANNEL_PREFIX}${configurationData.sourceTrackableId}`);
+  if (opts.verbose) console.log('Source channel:', sourceChannel.name);
+  let destinationChannel = isDestinationSpecified
+    ? destinationAbly.channels.get(`${CHANNEL_PREFIX}${configurationData.destinationTrackableId}`)
+    : sourceChannel;
+  if (opts.verbose) console.log('Destination channel:', destinationChannel.name);
 
   if (opts.verbose) console.log('Downloading location data from Ably channel history...');
-  const messages = await getEnhancedLocationsFromChannelHistory(channel);
+  const messages = await getEnhancedLocationsFromChannelHistory(sourceChannel);
   if (opts.verbose) {
     console.log('Location data downloaded');
     console.log(messages);
   }
 
   if (opts.verbose) console.log('Entering presence...');
-  await channel.presence.enter(PRESENCE_DATA);
+  await destinationChannel.presence.enter(PRESENCE_DATA);
   if (opts.verbose) console.log('Presence entered');
 
   if (opts.verbose) console.log(`Publishing location messages...`);
   for (let i = 0; i < messages.length; i++) {
     console.log(`Publishing location ${i}`);
     const message = messages[i];
-    await channel.publish(ENHANCED_LOCATION_MESSAGE, JSON.stringify(message));
+    await destinationChannel.publish(ENHANCED_LOCATION_MESSAGE, JSON.stringify(message));
     const isLastMessage = i === messages.length - 1;
     if (!isLastMessage) {
       const nextMessage = messages[i + 1];
@@ -64,10 +81,13 @@ if (opts.verbose) {
   if (opts.verbose) console.log(`Finished publishing location messages`);
 
   if (opts.verbose) console.log('Leaving presence...');
-  await channel.presence.leave(PRESENCE_DATA);
+  await destinationChannel.presence.leave(PRESENCE_DATA);
   if (opts.verbose) console.log('Presence left');
 
-  ably.close();
+  sourceAbly.close();
+  if (isDestinationSpecified) {
+    destinationAbly.close();
+  }
 
   process.exit(0);
 })();
