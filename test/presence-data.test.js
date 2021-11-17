1b36b35305a122af0bced8dbcac8feccf85ad83d
Kacper Kluka <k.kluka@nomtek.com>
2021-11-17 09:23:23 +0100

    Move resolution schema to a separate file

--- test/presence-data.test.js
+++ test/presence-data.test.js
@@ -1,12 +1,18 @@
 const expect = require('chai').expect;
 const fs = require('fs');
 const path = require('path');
-const jsonschema = require('jsonschema');
+const { Validator } = require('jsonschema');
 
 const geoDir = path.resolve(__dirname, '..', 'test-resources', 'geo');
 const exampleDir = path.resolve(geoDir, 'presence-data');
 const examples = fs.readdirSync(exampleDir);
 const schema = require(path.resolve(geoDir, 'presence-data-schema.json'));
+
+const jsonschema = new Validator();
+jsonschema.addSchema(
+  JSON.parse(fs.readFileSync(path.resolve(geoDir, 'resolution-schema.json'))),
+  'https://schemas.ably.com/json/asset-tracking-common/Resolution'
+);
 
 describe('Presence data schema', () => {
   examples.forEach((fileName) => {
