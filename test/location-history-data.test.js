5337aec9e3dbf9b20e178bd90600e67d2f1eee8d
Lawrence Forooghian <lawrence@forooghian.com>
2022-10-25 15:58:41 -0300

    Add a JSON schema for the LocationHistoryData type
    Taken from the Android model [1].
    
    I tested it with all of the data that had been uploaded from the Android
    publisher example app to the S3 bucket (the data at the top level of the
    bucket, not the ones in the "archive" directory).
    
    The valid-android.json is file taken from the 1_2022-10-19_10/07/41 file
    in that bucket (with the coordinates changed for privacy of whoever
    recorded the data).
    
    Closes #62.
    
    [1] https://github.com/ably/ably-asset-tracking-android/blob/8016b0fb97be04332d0c4d39404933a944d444fc/publishing-sdk/src/main/java/com/ably/tracking/publisher/LocationHistoryModels.kt#L12-L18

---
+++ test/location-history-data.test.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+const { Validator } = require('jsonschema');
+
+const geoDir = path.resolve(__dirname, '..', 'test-resources', 'geo');
+const exampleDir = path.resolve(geoDir, 'location-history-data');
+const examples = fs.readdirSync(exampleDir);
+const schema = require(path.resolve(geoDir, 'location-history-data-schema.json'));
+
+const jsonschema = new Validator();
+jsonschema.addSchema(
+  JSON.parse(fs.readFileSync(path.resolve(geoDir, 'location-schema.json'))),
+  'https://schemas.ably.com/json/asset-tracking-common/Location'
+);
+
+describe('Location history data schema', () => {
+  examples.forEach((fileName) => {
+    console.log(`filename ${fileName}`);
+    const instance = JSON.parse(fs.readFileSync(path.resolve(exampleDir, fileName)));
+
+    it(fileName, () => {
+      const { errors } = jsonschema.validate(instance, schema);
+      if (fileName.split('-')[0] === 'invalid') {
+        expect(errors.length).to.be.greaterThan(0);
+      } else {
+        expect(errors.length).to.equal(0);
+      }
+    });
+  });
+});
