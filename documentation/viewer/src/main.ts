1417384d7afbe21b26e3ec72be8feb5fad80afa5
Lawrence Forooghian <lawrence@forooghian.com>
2023-02-13 23:02:46 -0300

    Use Nunjucks as view engine for app created in 93d6b50

--- documentation/viewer/src/main.ts
+++ documentation/viewer/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { configure } from 'nunjucks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  configure(
+    join(
+      __dirname,
+      '..',
+      'views',
+    ) /* This code is running from the `dist` dir */,
+    {
+      autoescape: true,
+      express: app,
+    },
+  );
+
+  app.setViewEngine('njk');
+
   await app.listen(3000);
 }
 bootstrap();
